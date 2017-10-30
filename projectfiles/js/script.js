28db4b14aded2b97e666ce22bea8263d544d5db5
Robert Raimondi <robertraimondi@gmail.com>
2017-10-30 12:21:43 -0700

    "update js"

--- projectfiles/js/script.js
+++ projectfiles/js/script.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
     
     
+    /* for sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+       if (direction == "down") {
+           $('nav').addClass('sticky');
+       } else {
+           $('nav').removeClass('sticky');
+       }
+       
+    }, {
+      offset: '95px'
+    })
     
     
-    
+    /* Scroll to buttons */
+    $('.js--scroll-to-plans').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+    });
+
     
 });
+
