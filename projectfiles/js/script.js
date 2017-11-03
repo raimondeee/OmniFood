f4d19b8fa87ed63cfacb81be9b276e7174e6b350
Robert Raimondi <robertraimondi@gmail.com>
2017-11-03 09:18:56 -0700

    "update"

--- projectfiles/js/script.js
+++ projectfiles/js/script.js
@@ -17,8 +17,31 @@ $(document).ready(function() {
     /* Scroll to buttons */
     $('.js--scroll-to-plans').click(function () {
        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+    });    
+    
+    
+    $('.js--scroll-to-start').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
     });
+    
 
+    /* Navigation Scroll */
+    
+    $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html, body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+            }   
+        }
+    });
+    
+    
     
 });
 
